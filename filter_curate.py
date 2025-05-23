45b85dc58f6a22779cd411fda92e57bdae9af577
fcogidi <41602287+fcogidi@users.noreply.github.com>
2025-05-23 10:01:54 -0400

    refactor: enhance keyword extraction from LLM and streamline experiment data curation

--- filter_curate.py
+++ filter_curate.py
@@ -1,19 +1,24 @@
+import logging
 import os
 from typing import Union
+
+import hydra
 import yaml
 from dotenv import load_dotenv
-import hydra
 from hydra.utils import instantiate
 from omegaconf import DictConfig
 
 from create_study import Study
 from naturalv2.evals.experiment import Experiment
+from naturalv2.models.lm import build_lm_instance_from_cfg, extract_list_response
 from naturalv2.sources.pubmed import PubMedSet
 from naturalv2.sources.reddit import RedditSource
+from naturalv2.utils import ListResponse, load_prompt
 
-# ruff: noqa
 
 load_dotenv(".env")
+
+logger = logging.getLogger(__name__)
 
 
 class StudyDataset:
@@ -40,10 +45,37 @@ class StudyDataset:
         return study_dataset
 
 
+def get_keywords_from_llm(study: Study, lm_cfg: DictConfig) -> list[str]:
+    lm = build_lm_instance_from_cfg(lm_cfg)
+    condition = study.conditions[0]
+
+    messages: list[dict[str, str]] = load_prompt(
+        base_dir="naturalv2/prompts",
+        prompt_type="conditions_keywords",
+        return_format="messages",
+        condition=condition,
+    )
+
+    response = lm.call_sync(messages=messages, response_format=ListResponse)
+    keywords = extract_list_response(response)
+    if keywords:
+        final_keywords = keywords[0]
+        logger.info(
+            f"LLM extracted the following keywords for {condition}: {final_keywords}"
+        )
+        return final_keywords
+
+    raise ValueError(
+        "The LLM did not return any keywords. Please check the model and the prompt."
+    )
+
+
 @hydra.main(config_path="conf/", config_name="config.yaml", version_base="1.2")
 def main(cfg: DictConfig) -> None:
     study_file = os.path.join(
-        cfg.save_path, "studies", cfg.conditions[0] + "_study.yaml"
+        cfg.save_path,
+        "studies",
+        cfg.conditions[0].lower().replace(" ", "_") + "_study.yaml",
     )
     study = Study.from_yaml(study_file)
     train_ncts = [list(trial.keys())[0] for trial in study.train_trials]
@@ -58,12 +90,41 @@ def main(cfg: DictConfig) -> None:
     else:
         study_dataset = StudyDataset(study.conditions, cfg.sources)
 
+    def curate_exp_data(nct_id, split, source_name):
+        exp_file = os.path.join(cfg.save_path, f"experiments/{nct_id}.yaml")
+        # Load Experiment from exisiting file or create a new one
+        try:
+            exp = Experiment.from_yaml(exp_file)
+        except:
+            status = "active" if split == "test" else "completed"
+            exp = Experiment(cfg.data_path, nct_id, status=status)
+        # Track the studies of which this Experiment is a part
+        if cfg.conditions[0] not in exp.studies:
+            exp.studies.append([cfg.conditions[0], split])
+        # Curate a dataset for this Experiment from {source_name}
+        for attribute in ["treatment", "outcome"]:
+            if source_name not in getattr(exp, f"{attribute}_common_names"):
+                exp.set_common_names(
+                    attribute,
+                    source_name,
+                    cfg.sample_model,
+                    source_dataset.get_common_name_prompts(),
+                )
+        exp_data_path, exp_data_size = source_dataset.experiment_data(
+            exp, study.conditions[0], cfg.filter_by_date, clean_path
+        )
+        # Track Experiment data and save to yaml
+        exp.source_paths[source_name].append(exp_data_path)
+        exp.to_yaml(exp_file)
+        return exp_data_path, exp_data_size
+
     for source_name in cfg.sources:
         source_dataset: Union[RedditSource, PubMedSet] = instantiate(cfg[source_name])
 
         # search for keyword list + download
         if f"{source_name}_condition_filtered" not in study_dataset.data_paths:
-            condition_filter_paths = source_dataset.condition_filter(study.conditions)
+            keywords = get_keywords_from_llm(study, cfg[source_name].lm_cfg)
+            condition_filter_paths = source_dataset.condition_filter(keywords)
             study_dataset.data_paths.update(
                 {f"{source_name}_condition_filtered": condition_filter_paths}
             )
@@ -78,34 +139,6 @@ def main(cfg: DictConfig) -> None:
         # Search for treatment and outcome to curate data for each experiment
         os.makedirs(os.path.join(cfg.save_path, "experiments"), exist_ok=True)
 
-        def curate_exp_data(nct_id, split):
-            exp_file = os.path.join(cfg.save_path, f"experiments/{nct_id}.yaml")
-            # Load Experiment from exisiting file or create a new one
-            try:
-                exp = Experiment.from_yaml(exp_file)
-            except:
-                status = "active" if split == "test" else "completed"
-                exp = Experiment(cfg.data_path, nct_id, status=status)
-            # Track the studies of which this Experiment is a part
-            if cfg.conditions[0] not in exp.studies:
-                exp.studies.append([cfg.conditions[0], split])
-            # Curate a dataset for this Experiment from {source_name}
-            for attribute in ["treatment", "outcome"]:
-                if source_name not in getattr(exp, f"{attribute}_common_names"):
-                    exp.set_common_names(
-                        attribute,
-                        source_name,
-                        cfg.sample_model,
-                        source_dataset.get_common_name_prompts(),
-                    )
-            exp_data_path, exp_data_size = source_dataset.experiment_data(
-                exp, study.conditions[0], cfg.filter_by_date, clean_path
-            )
-            # Track Experiment data and save to yaml
-            exp.source_paths[source_name].append(exp_data_path)
-            exp.to_yaml(exp_file)
-            return exp_data_path, exp_data_size
-
         splits = (
             ["train" for _ in range(len(train_ncts))]
             + ["val" for _ in range(len(val_ncts))]
@@ -113,7 +146,9 @@ def main(cfg: DictConfig) -> None:
         )
         for nct_id, split in zip(train_ncts + val_ncts + test_ncts, splits):
             if f"{source_name}_{nct_id}" not in study_dataset.data_paths:
-                exp_data_path, exp_data_size = curate_exp_data(nct_id, split)
+                exp_data_path, exp_data_size = curate_exp_data(
+                    nct_id, split, source_name
+                )
                 # Track Experiment data in study dataset
                 study_dataset.data_paths.update(
                     {f"{source_name}_{nct_id}": exp_data_path}

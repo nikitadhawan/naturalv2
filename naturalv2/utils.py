a783ea072c50a4cf053d43b1260e1340ea203100
fcogidi <41602287+fcogidi@users.noreply.github.com>
2025-05-23 09:57:24 -0400

    refactor: replace text prompt files with JSON formatted prompts

--- naturalv2/utils.py
+++ naturalv2/utils.py
@@ -1,3 +1,5 @@
+import json
+import logging
 import os
 import re
 from typing import Any, Dict, List, Literal, Optional, Type, Union
@@ -11,6 +13,9 @@ from naturalv2.evals.clinical_trial import (
     DesignAllocation,
     Outcome,
 )
+
+
+logger = logging.getLogger(__name__)
 
 
 class ListResponse(BaseModel):
@@ -76,9 +81,70 @@ class ImputationsResponse(BaseModel):
     duration_days: Union[int, Literal["Unknown"]]
 
 
-def load_prompt(base_dir, prompt_type):
-    with open(os.path.join(base_dir, f"{prompt_type}.txt"), "r") as f:
-        return f.read()
+def load_prompt(
+    base_dir: str,
+    prompt_type: str,
+    return_format: Optional[Literal["messages", "prompt"]] = None,
+    **user_prompt_format_kwargs: dict[str, str],
+) -> Union[str, list[dict[str, str]]]:
+    if return_format not in ["messages", "prompt", None]:
+        raise ValueError("return_format must be either 'messages', 'prompt', or None.")
+
+    filepath = os.path.join(base_dir, f"{prompt_type}.json")
+    if not os.path.exists(filepath):
+        raise FileNotFoundError(f"Prompt file {filepath} not found.")
+
+    # load json file
+    with open(filepath, "r") as f:
+        prompt_data: dict[str, Any] = json.load(f)
+
+    # 'user_prompt_template' must be in the json file
+    if "user_prompt_template" not in prompt_data:
+        raise KeyError(
+            f"'user_prompt_template' key not found in {prompt_type}.json file."
+            "Please check the file format."
+        )
+
+    # 'system_prompt_template' is optional, but if it exists, construct and return
+    # a list of dictionaries with 'role' and 'content' keys
+    system_prompt: Optional[str] = prompt_data.get("system_prompt_template")
+    if system_prompt:
+        logger.debug(f"System prompt loaded from {prompt_type}.json: {system_prompt}")
+
+        system_role_dict = {"role": "system", "content": system_prompt}
+
+    user_prompt_template: str = prompt_data["user_prompt_template"].format(
+        **user_prompt_format_kwargs
+    )
+    logger.debug(
+        f"User prompt template loaded from {prompt_type}.json: {user_prompt_template}"
+    )
+
+    if "examples_intro" in prompt_data and (
+        "examples" in prompt_data and len(prompt_data["examples"]) > 0
+    ):
+        examples_intro = prompt_data["examples_intro"]
+        examples: list[dict[str, str]] = prompt_data["examples"]
+
+        # append examples intro and examples to user_prompt_template
+        user_prompt_template += f"\n\n{examples_intro}"
+        for example in examples:
+            user_prompt_template += f"\n\n{example['input']}\n\n{example['output']}"
+
+    if return_format in ["messages", None]:
+        user_role_dict = {"role": "user", "content": user_prompt_template}
+
+        if system_prompt:
+            return [system_role_dict, user_role_dict]
+        if return_format == "messages":
+            return [user_role_dict]
+
+    # concatenate system_prompt and user_prompt_template
+    # and return as a single string
+    if system_prompt:
+        return f"{system_prompt}\n\n{user_prompt_template}"
+
+    return user_prompt_template
 
 
 def get_save_path(
